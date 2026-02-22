9ea43c3da248a1765a80a9282ac55fb770f5cbe2
Ahmad Maher <258445538+zmaherdev@users.noreply.github.com>
2026-02-22 17:34:30 +0200

    feat: Data types overview

---
+++ data_types.py
@@ -0,0 +1,19 @@
+print(type(10)) # Integer
+print(type(1000)) # Integer
+print(type(-40)) # Integer
+
+print(type(-40.5)) # Floating Point Number
+print(type(4.929925)) # Floating Point Number
+print(type(4000.112)) # Floating Point Number
+
+print(type("Hello there!")) # String
+
+print(type( [1,2,3,5] )) # List
+
+print(type( (1,2,3,5) )) # Tuple
+
+print(type( {"One": 1, "Two": 2, "Three": 3} )) # Dictionary
+
+print(type(2 == 2)) # Boolean
+print(type(True)) # Boolean
+print(type(False)) # Boolean
