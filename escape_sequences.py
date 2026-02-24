313f932b6b0086774faa8e7781145c9562881bc5
Ahmad Maher <258445538+zmaherdev@users.noreply.github.com>
2026-02-24 19:02:04 +0200

    feat: Escape Sequences

---
+++ escape_sequences.py
@@ -0,0 +1,19 @@
+# Escape newline
+print("Hello \
+This \
+Works!")
+
+# Insert backspace
+print("Now\b World")
+
+# Insert line break
+print("That's\nAwesome")
+
+# Carriage return
+print("122995\rHello")
+
+# Tab
+print("One\tTwo")
+
+# Character hex value
+print("\xA1\xB2")
