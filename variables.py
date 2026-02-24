786c1e284a77240f2386efbcd82d06ff84a15f3f
Ahmad Maher <258445538+zmaherdev@users.noreply.github.com>
2026-02-24 18:42:13 +0200

    feat: Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+name = "Ahmad Maher"
+number = 2
+
+print(name)
+print(type(number))
+
+# Print reserved words
+help("keywords")
+
+# Assign multiple variables
+x, y, z = 3, 4, 9
+
+print(x)
+print(y)
+print(z)
